22ba3af832001ced570062f7d89323cfd2b75f96
Chamon <luizchamon@gmail.com>
2015-07-16 16:50:19 -0300

    Clean up code and fix coding standards.
    Signed-off-by: Chamon <luizchamon@gmail.com>

--- main.c
+++ main.c
@@ -28,7 +28,8 @@
 
 
 // Function to load the shared RAM memory information from sysfs
-int getMemInfo(unsigned int *addr, unsigned int *size){
+int getMemInfo(unsigned int *addr, unsigned int *size)
+{
     FILE* pfile;
 
     // Read shared RAM address
@@ -46,7 +47,8 @@ int getMemInfo(unsigned int *addr, unsigned int *size){
 
 
 /* Handshake with client */
-int client_handshake(int clientSocket){
+int client_handshake(int clientSocket)
+{
     char readBuffer[HANDSHAKE_SIZE + 1];
 
     /* Clear receive buffer */
@@ -54,20 +56,19 @@ int client_handshake(int clientSocket){
 
     /* Wait for client handshake */
     receiveData(clientSocket, readBuffer, HANDSHAKE_SIZE);
-    while(strcmp(readBuffer, "") == 0){
+    while (strcmp(readBuffer, "") == 0)
         receiveData(clientSocket, readBuffer, HANDSHAKE_SIZE);
-    }
 
     /* Check handshake content */
-    if(strcmp(readBuffer, "Ready") != 0){
+    if (strcmp(readBuffer, "Ready") != 0)
         return(-1);
-    }
 
     return(0);
 }
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int i;
 
     int fd;
@@ -82,7 +83,7 @@ int main(int argc, char *argv[]){
 
 
     /* PRU code only works if executed as root */
-    if(getuid() != 0){
+    if (getuid() != 0) {
         fprintf(stderr, "This program needs to run as root.\n");
         exit(EXIT_FAILURE);
     }
@@ -94,14 +95,14 @@ int main(int argc, char *argv[]){
 
     /* Get shared RAM information */
     getMemInfo(&addr, &size);
-    if(size < BUFFER_SIZE) {
+    if (size < BUFFER_SIZE) {
         printf("error.\n");
         fprintf(stderr, "External RAM pool must be at least %d bytes.\n", BUFFER_SIZE);
         exit(EXIT_FAILURE);
     }
 
     /* Get access to device memory */
-    if((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1){
+    if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
         printf("error.\n");
         perror("Failed to open memory!");
         exit(EXIT_FAILURE);
@@ -113,10 +114,9 @@ int main(int argc, char *argv[]){
     /* Close file descriptor (not needed after memory mapping) */
     close(fd);
 
-    if(mem_map == (void *) -1) {
+    if (mem_map == (void *) -1) {
         printf("error.\n");
         perror("Failed to map base address");
-        close(fd);
         exit(EXIT_FAILURE);
     }
 
@@ -135,9 +135,8 @@ int main(int argc, char *argv[]){
 
     /* Attempt to improve performance */
     n = 1;
-    if(setsockopt(clientSocket, IPPROTO_TCP, TCP_NODELAY, &n, sizeof(int)) == -1){
+    if (setsockopt(clientSocket, IPPROTO_TCP, TCP_NODELAY, &n, sizeof(int)) == -1)
         perror("Error setting TCP_NODELAY socket option. Will try to go on anyway...\n");
-    }
 
     /* Handshake with client */
     printf("Client handshake... ");
@@ -155,7 +154,9 @@ int main(int argc, char *argv[]){
 
     /***** PRU SET UP *****/
     printf("Setting up PRUs... ");
-    if(pru_setup() != 0) {
+    fflush(stdout);
+
+    if (pru_setup() != 0) {
         printf("error.\n");
         fprintf(stderr, "Error setting up the PRU.\n");
         pru_cleanup();
@@ -192,7 +193,7 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
-    for(i = 0; i < N_BUFFER; i++) {
+    for (i = 0; i < N_BUFFER; i++) {
         /* Wait for PRU_EVTOUT_0 and send shared RAM data */
         prussdrv_pru_wait_event(PRU_EVTOUT_0);
         prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
@@ -206,13 +207,13 @@ int main(int argc, char *argv[]){
 
 
     /* PRU CLEAN UP */
-    printf("\nStopping PRUs.\n");
+    printf("Stopping PRUs.\n");
     pru_stop(PRU1);
     pru_stop(PRU0);
     pru_cleanup();
 
     printf("Dumpping buffer.\n");
-    fp_buf = fopen("buf.txt", "w");
+    fp_buf = fopen("buff", "w");
     fwrite(ram_addr, sizeof(uint32_t), 2*MSG_SIZE/4, fp_buf);
     fclose(fp_buf);
 
@@ -221,11 +222,10 @@ int main(int argc, char *argv[]){
     close(clientSocket);
 
     /* SHARED RAM CLEAN UP */
-    if(munmap(mem_map, RAM_SIZE) == -1) {
+    if (munmap(mem_map, RAM_SIZE) == -1) {
         perror("Failed to unmap memory");
         exit(EXIT_FAILURE);
     }
-    close(fd);
 
     return(EXIT_SUCCESS);
 }
--- pru/pru.c
+++ pru/pru.c
@@ -2,32 +2,32 @@
 
 int pru_setup()
 {
-  /* Initialize the PRU and prussdrv module (always returns 0) */
-  prussdrv_init();
+    /* Initialize the PRU and prussdrv module (always returns 0) */
+    prussdrv_init();
 
-  /* Open interrupt(s): you should open at least PRU_EVTOUT_0 */
-  if (prussdrv_open(PRU_EVTOUT_0) != 0) {
-    fprintf(stderr, "There was an error while opening PRU_EVTOUT_0.\n");
-    fprintf(stderr, "Did you load the PRU DTO?\n");
-    return(-1);
-  }
+    /* Open interrupt(s): you should open at least PRU_EVTOUT_0 */
+    if (prussdrv_open(PRU_EVTOUT_0) != 0) {
+        fprintf(stderr, "There was an error while opening PRU_EVTOUT_0.\n");
+        fprintf(stderr, "Did you load the PRU DTO?\n");
+        return(-1);
+    }
 
-  if (prussdrv_open(PRU_EVTOUT_1) != 0) {
-    fprintf(stderr, "There was an error while opening PRU_EVTOUT_1.\n");
-    fprintf(stderr, "Did you load the PRU DTO?\n");
-    return(-1);
-  }
+    if (prussdrv_open(PRU_EVTOUT_1) != 0) {
+        fprintf(stderr, "There was an error while opening PRU_EVTOUT_1.\n");
+        fprintf(stderr, "Did you load the PRU DTO?\n");
+        return(-1);
+    }
 
-  /* Initialize struct with INTC interrupt map (see PRU ref. guide, Fig. 97) */
-  tpruss_intc_initdata pru_intc_data = PRUSS_INTC_INITDATA;
+    /* Initialize struct with INTC interrupt map (see PRU ref. guide, Fig. 97) */
+    tpruss_intc_initdata pru_intc_data = PRUSS_INTC_INITDATA;
 
-  /* Initialize INTC */
-  if (prussdrv_pruintc_init(&pru_intc_data) != 0) {
-    return(-1);
-    fprintf(stderr, "An error occurred while initializing INTC.");
-  }
+    /* Initialize INTC */
+    if (prussdrv_pruintc_init(&pru_intc_data) != 0) {
+        return(-1);
+        fprintf(stderr, "An error occurred while initializing INTC.");
+    }
 
-  return(0);
+    return(0);
 }
 
 
@@ -49,30 +49,30 @@ int pru_mmap(int pru_number, uint32_t **pru_mem)
 
 int pru_start(int pru_number, char *program)
 {
-  /* Load and execute the PRU program */
-  if (prussdrv_exec_program(pru_number, program) != 0) {
-    return(-1);
-  }
+    /* Load and execute the PRU program */
+    if (prussdrv_exec_program(pru_number, program) != 0) {
+        return(-1);
+    }
 
-  return(0);
+    return(0);
 }
 
 
 int pru_stop(int pru_number)
 {
-  /* Halt and disable the PRU (returns -1 if argument is not 0 or 1) */
-  if (prussdrv_pru_disable(pru_number) != 0) {
-    fprintf(stderr, "The argument passed to prussdrv_pru_disable() must be 0 or 1.\n");
-    return(-1);
-  }
+    /* Halt and disable the PRU (returns -1 if argument is not 0 or 1) */
+    if (prussdrv_pru_disable(pru_number) != 0) {
+        fprintf(stderr, "The argument passed to prussdrv_pru_disable() must be 0 or 1.\n");
+        return(-1);
+    }
 
-  return(0);
+    return(0);
 }
 
 
 int pru_cleanup()
 {
-  /* Release PRU clocks and disable prussdrv module (always returns 0) */
-  prussdrv_exit();
-  return(0);
+    /* Release PRU clocks and disable prussdrv module (always returns 0) */
+    prussdrv_exit();
+    return(0);
 }
